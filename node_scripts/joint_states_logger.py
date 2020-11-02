ae108c94fccc3a7c246c7809b7390c1c3beb12ec
Shingo Kitagawa <shingogo@hotmail.co.jp>
2020-11-02 17:37:31 +0900

    use threading in joint_states_logger

--- node_scripts/joint_states_logger.py
+++ node_scripts/joint_states_logger.py
@@ -2,6 +2,7 @@
 
 import influxdb
 import rospy
+import threading
 
 from influxdb_store.utils import timestamp_to_influxdb_time
 
@@ -17,7 +18,10 @@ class JointStatesLogger(object):
             host=host, port=port, database=database)
         self.client.create_database(database)
         self.sub = rospy.Subscriber(
-            '~input', JointState, self._cb, queue_size=1)
+            '~input', JointState, self._cb, queue_size=100)
+        self.timer = rospy.Timer(rospy.Duration(3.0), self._timer_cb)
+        self.lock = threading.Lock()
+        self.query = []
 
     def _cb(self, msg):
         time = timestamp_to_influxdb_time(msg.header.stamp)
@@ -25,23 +29,28 @@ class JointStatesLogger(object):
         position = msg.position
         velocity = msg.velocity
         effort = msg.effort
-        query = []
         for joint_name, pos, vel, eff in zip(
                 joint_names, position, velocity, effort):
-            query.append({
-                "measurement": "joint_states",
-                "tags": {
-                    "joint_name": joint_name
-                },
-                "time": time,
-                "fields": {
-                    "position": pos,
-                    "velocity": vel,
-                    "effort": eff
-                }
-            })
-        if len(query) > 0:
-            self.client.write_points(query, time_precision='ms')
+            with self.lock:
+                self.query.append({
+                    "measurement": "joint_states",
+                    "tags": {
+                        "joint_name": joint_name
+                    },
+                    "time": time,
+                    "fields": {
+                        "position": pos,
+                        "velocity": vel,
+                        "effort": eff
+                    }
+                })
+
+    def _timer_cb(self, event):
+        with self.lock:
+            if len(self.query) == 0:
+                return
+            self.client.write_points(self.query, time_precision='ms')
+            self.query = []
 
 
 if __name__ == '__main__':
