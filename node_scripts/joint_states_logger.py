0be8e21323e4b053cb9bac3a7cb57eac7bea74db
Shingo Kitagawa <shingogo@hotmail.co.jp>
2020-11-14 17:06:53 +0900

    update joint_states_logger.py

--- node_scripts/joint_states_logger.py
+++ node_scripts/joint_states_logger.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+import copy
 import influxdb
 import rospy
 import threading
+import time
 
 from influxdb_store.utils import timestamp_to_influxdb_time
 
@@ -14,43 +16,64 @@ class JointStatesLogger(object):
         host = rospy.get_param('~host', 'localhost')
         port = rospy.get_param('~port', 8086)
         database = rospy.get_param('~database', 'test')
+        self.duration = rospy.get_param('~duration', 3.0)
         self.client = influxdb.InfluxDBClient(
             host=host, port=port, database=database)
         self.client.create_database(database)
         self.sub = rospy.Subscriber(
-            '~input', JointState, self._cb, queue_size=100)
-        self.timer = rospy.Timer(rospy.Duration(3.0), self._timer_cb)
+            '~input', JointState, self._cb, queue_size=300)
+        self.timer = rospy.Timer(rospy.Duration(self.duration), self._timer_cb)
         self.lock = threading.Lock()
         self.query = []
 
     def _cb(self, msg):
-        time = timestamp_to_influxdb_time(msg.header.stamp)
+        influx_time = timestamp_to_influxdb_time(msg.header.stamp)
         joint_names = msg.name
         position = msg.position
         velocity = msg.velocity
         effort = msg.effort
-        for joint_name, pos, vel, eff in zip(
-                joint_names, position, velocity, effort):
-            with self.lock:
-                self.query.append({
-                    "measurement": "joint_states",
-                    "tags": {
-                        "joint_name": joint_name
-                    },
-                    "time": time,
-                    "fields": {
-                        "position": pos,
-                        "velocity": vel,
-                        "effort": eff
-                    }
-                })
+        with self.lock:
+            self.query.append({
+                "measurement": "joint_states",
+                "tags": {
+                    "type": "position"
+                },
+                "time": influx_time,
+                "fields": dict(zip(joint_names, position))
+            })
+            self.query.append({
+                "measurement": "joint_states",
+                "tags": {
+                    "type": "velocity"
+                },
+                "time": influx_time,
+                "fields": dict(zip(joint_names, velocity))
+            })
+            self.query.append({
+                "measurement": "joint_states",
+                "tags": {
+                    "type": "effort"
+                },
+                "time": influx_time,
+                "fields": dict(zip(joint_names, effort))
+            })
 
     def _timer_cb(self, event):
+        start_time = time.time() * 1000
         with self.lock:
             if len(self.query) == 0:
                 return
-            self.client.write_points(self.query, time_precision='ms')
+            query = copy.deepcopy(self.query)
             self.query = []
+        end_time = time.time() * 1000
+        rospy.logdebug("copy time: {}ms".format(end_time - start_time))
+        rospy.logdebug("data length: {}ms".format(len(query)))
+        self.client.write_points(query, time_precision='ms')
+        end_time = time.time() * 1000
+        rospy.logdebug("timer cb time: {}ms".format(end_time - start_time))
+        if ((end_time - start_time) > (self.duration * 1000)):
+            rospy.logerr("timer cb time exceeds: {} > {}".format(
+                end_time - start_time, self.duration * 1000))
 
 
 if __name__ == '__main__':
