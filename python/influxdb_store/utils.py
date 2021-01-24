acf8703318ab0424d12d672c86fe2cdc44b43111
Shingo Kitagawa <shingogo@hotmail.co.jp>
2021-01-24 19:33:07 +0900

    use pytz

--- python/influxdb_store/utils.py
+++ python/influxdb_store/utils.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+import pytz
 
 
 def timestamp_to_influxdb_time(timestamp):
     time = datetime.utcfromtimestamp(timestamp.to_sec())
-    time = time.isoformat("T") + "Z"
+    time = pytz.timezone('UTC').localize(time).isoformat('T')
     return time
